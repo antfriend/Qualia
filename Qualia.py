db97ec0694ae3155f654098a5fae3473bad1e290
antfriend <antfriend@gmail.com>
2022-10-28 20:13:40 -0600

    Update Qualia.py

--- Qualia.py
+++ Qualia.py
@@ -4,7 +4,7 @@ import keyboard
 import time
 import subprocess
 subprocess.run('', shell=True)
-# 
+
 os.system("cls")
 
 W  = '\033[0m'  # white (normal)
@@ -13,9 +13,6 @@ G  = '\033[32m' # green
 O  = '\033[33m' # orange
 B  = '\033[34m' # blue
 P  = '\033[35m' # purple
-X = '\0338' # set position?
-
-loopies = 0
 
 def move (y, x):
     print('\033[%d;%dH' % (y, x))
@@ -29,21 +26,25 @@ def blink():
     print(' ')
     time.sleep(.1)
 
+def setup():
+    blink()
+    print(' ')
 
-blink()
-print(' ')
+def loop():
+    loopies = 0q
+    while True:
+        loopies = loopies + 1
+        time.sleep(.01)
+        if loopies > 100:
+            loopies = 0
+            blink()
+
+        if keyboard.is_pressed("q"):
+            print("You pressed q")
+            break
+
+setup()
+loop()
+
 print(W+'i r Qualia ')
 print(' ')
-blink()
-
-
-while True:
-    loopies = loopies + 1
-    time.sleep(.0q1)
-    if loopies > 100:
-        loopies = 0
-        blink()
-
-    if keyboard.is_pressed("q"):
-        print("You pressed q")
-        break
